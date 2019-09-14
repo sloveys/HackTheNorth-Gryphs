3551eb276c391c994a403aa228fbfe8f01fc3130
Kinh Tran <kinh@uoguelph.ca>
2019-09-14 09:26:58 -0400

    Added VR integration with cardboard, sky box, and model

---
+++ FaceChat/Assets/FlipNormals.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FlipNormals : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        mesh.normals.Select(normal => normal * -1);
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+}
