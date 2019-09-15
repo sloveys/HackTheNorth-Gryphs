8a1010740cb2e4593a7cfea0c35e46a8c42defbd
sloveys <samuel_loveys@outlook.com>
2019-09-14 21:10:34 -0400

    added the things

---
+++ FaceChat/Assets/PlayListener.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayListener : MonoBehaviour
+{
+  public void PlayClick()
+  {
+    UnityEngine.XR.XRSettings.enabled = true;
+    SceneManager.LoadScene(1, LoadSceneMode.Single);
+  }
+}
