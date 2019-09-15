d75373ab2b6ee5043cb5301038b70b4489944af0
Kinh Tran <kinh@uoguelph.ca>
2019-09-15 03:23:12 -0400

    multiplayer works

---
+++ FaceChat/Assets/Friends.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Example : NetworkBehaviour
+{
+    //Assign the prefab in the Inspector
+    public GameObject m_MyGameObject;
+    GameObject m_MyInstantiated;
+
+    void Start()
+    {
+        //Instantiate the prefab
+        m_MyInstantiated = Instantiate(m_MyGameObject);
+        NetworkServer.Spawn(m_MyInstantiated);
+    }
+}
